82558fee83d294f4a0232bd3fa9e76b9429cd907
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-04-28 21:29:14 +0700

    Таймер для класса Game.

--- src/com/github/svyaz/minesweeper/Main.java
+++ src/com/github/svyaz/minesweeper/Main.java
@@ -1,14 +1,12 @@
 package com.github.svyaz.minesweeper;
 
-import com.github.svyaz.minesweeper.gamemodel.gamemodes.GameMode;
+import com.github.svyaz.minesweeper.gamemodel.Game;
 import com.github.svyaz.minesweeper.gamemodel.gamemodes.RookieMode;
 
 public class Main {
     public static void main(String[] args) {
-        //Field field = new Field(new GameMode("Beginner", 9,9,10));
-        //field.printField();
-        GameMode mode = new RookieMode();
-        System.out.println(mode.getBombsCount());
+
+        Game game = new Game(new RookieMode());
     }
 }
 
--- src/com/github/svyaz/minesweeper/gamemodel/Game.java
+++ src/com/github/svyaz/minesweeper/gamemodel/Game.java
@@ -2,7 +2,9 @@ package com.github.svyaz.minesweeper.gamemodel;
 
 import com.github.svyaz.minesweeper.gamemodel.gamemodes.GameMode;
 
-import java.time.Duration;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class Game {
     /**
@@ -10,20 +12,50 @@ public class Game {
      * При создании новой игры сначала у нее статус NOT_STARTED.
      * Таймер начинает отсчитывать только после первого нажатия на игровое поле.
      */
-    GameStatus status = GameStatus.NOT_STARTED;
+    private GameStatus status;
+
+    /**
+     * Таймер игры.
+     */
+    private Timer timer;
 
     /**
      * Время игры.
      */
-    Duration duration;
+    private long time;
 
     /**
      * Режим игры: Новичок, Любитель, Профессионал, Настраиваемая игра.
      */
-    GameMode mode;
+    private GameMode gameMode;
 
     /**
      * Игровое поле.
      */
-    Field field;
+    private Field field;
+
+    /**
+     * Создание игры в выбранном режиме.
+     */
+    public Game(GameMode gameMode) {
+        this.status = GameStatus.NOT_STARTED;
+        this.gameMode = gameMode;
+        this.timer = new Timer();
+    }
+
+    /**
+     * Запуск таймера при старте игры
+     */
+    private void startTimer() {
+        TimerTask timerTask = new TimerTask() {
+            private final long startTime = System.currentTimeMillis();
+
+            @Override
+            public void run() {
+                //System.out.println((System.currentTimeMillis() - startTime) / 1000);
+                time = System.currentTimeMillis() - startTime;
+            }
+        };
+        timer.schedule(timerTask, 0, 1000);
+    }
 }
